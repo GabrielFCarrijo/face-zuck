a63be1309fb14dafe647cf712b056f758adc7178
Lucas Mesquita <lucas.mesquita@perinity.com>
2024-07-21 20:59:37 -0300

    ajustando imports

---
+++ src/main/java/br/com/faceZuck/Main.java
@@ -0,0 +1,24 @@
+package br.com.faceZuck;
+
+import io.quarkus.runtime.Quarkus;
+import io.quarkus.runtime.QuarkusApplication;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@QuarkusMain
+@ApplicationPath("/")
+public class Main extends Application {
+
+	public static void main(String... args) {
+		Quarkus.run(MyApp.class);
+	}
+	public static class MyApp implements QuarkusApplication {
+
+		@Override
+		public int run(String... args) throws Exception {
+			Quarkus.waitForExit();
+			return 0;
+		}
+	}
+}
