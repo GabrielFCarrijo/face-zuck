29ca223c791465d50abeb4dfaee5449785e3aacb
lucaskitinha <lucas.lmso@gmail.com>
2024-07-25 16:21:28 -0300

    feat: implementando login com o google

--- src/main/java/br/com/faceZuck/model/Register.java
+++ src/main/java/br/com/faceZuck/model/Register.java
@@ -21,7 +21,6 @@ public class Register {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     private String name;
     private String username;
     private LocalDate birthDate;
--- src/main/java/br/com/faceZuck/service/register/RegisterService.java
+++ src/main/java/br/com/faceZuck/service/register/RegisterService.java
@@ -7,11 +7,15 @@ import br.com.faceZuck.model.Register;
 import br.com.faceZuck.repository.RegisterRepository;
 import br.com.faceZuck.service.mapper.RegisterMapper;
 import br.com.faceZuck.service.register.registerValidation.ValidateRegisterUser;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.mindrot.jbcrypt.BCrypt;
 
 import java.time.LocalDate;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @ApplicationScoped
 public class RegisterService {
@@ -22,6 +26,10 @@ public class RegisterService {
     ValidateRegisterUser validateRegisterUser;
     @Inject
     RegisterMapper registerMapper;
+
+    private static final String PATTERN_SENHA = "abcdefghijklmnopqrstuvxz1234567890.:_-]}{[&*)(%$@!|^~";
+    private static final String REGEX = "^(?=.*[.:_&*)(%$@!|^~])(?=.*[\\d])(?=.*[a-zA-Z])[@!#$%^&*()\\[\\]{}/a-zA-Z0-9]{8,20}$";
+    private static final Integer TAMANHO_SENHA = 16;
 
     public void registerUser(RegisterForm registerForm) {
         validateRegisterUser.validateRegisterUser(registerForm);
@@ -42,6 +50,35 @@ public class RegisterService {
             throw new ValidationException("User not found");
         }
         return registerMapper.toDTO(register);
+    }
+
+    public void registerUserGoogle(GoogleIdToken.Payload payload) {
+        var registerForm = RegisterForm.builder()
+                .name((String) payload.get("name"))
+                .email(payload.getEmail())
+                .password(generateRandomPassword())
+                .build();
+
+        registerUser(registerForm);
+    }
+
+    public boolean checkIfRegisterExists(String email) {
+        var register = registerRepository.findByEmail(email);
+        return register != null;
+    }
+
+    private String generateRandomPassword() {
+        var random = RandomStringUtils.random(TAMANHO_SENHA, PATTERN_SENHA.toCharArray());
+        while (!validatePassword(random)) {
+            random = RandomStringUtils.random(TAMANHO_SENHA, PATTERN_SENHA.toCharArray());
+        }
+        return random;
+    }
+
+    private boolean validatePassword(String random) {
+        Pattern p = Pattern.compile(REGEX);
+        Matcher m = p.matcher(random);
+        return m.matches();
     }
 
 }
